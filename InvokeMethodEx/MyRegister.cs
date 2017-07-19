f9250544f3c4dacf2501ab19b6c7ae87c4f912d3
bartmao <maochen.mars@qq.com>
2017-07-19 10:50:34 +0800

    Some code changes

---
+++ InvokeMethodEx/MyRegister.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Activities.Presentation.Metadata;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitiesEx
+{
+    public class MyRegister : IRegisterMetadata
+    {
+        public void Register()
+        {
+            var attributeTableBuilder = new AttributeTableBuilder();
+            // Category attribute not working in UIPath, not known yet
+            attributeTableBuilder.AddCustomAttributes(typeof(InvokeMethodEx), new Attribute[]
+            {
+                new CategoryAttribute("Bart.Infrastructure")
+            });
+            MetadataStore.AddAttributeTable(attributeTableBuilder.CreateTable());
+        }
+    }
+}
